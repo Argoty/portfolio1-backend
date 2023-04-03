a0563419e504a9e6538483b4c895937376e8c9d4
JaxisArty <jaxis0810@gmail.com>
2023-04-03 14:15:36 -0500

    inicio API

---
+++ main.py
@@ -0,0 +1,46 @@
+# from routers import palabras, usuarios
+from fastapi import FastAPI, HTTPException
+
+# from db.models.vocabulario import Vocabulario, Palabra
+
+# from db.schemas.vocabulario import vocabularios_schema
+# from db.client import db_client
+
+# from buscador import search_vocabulario, search_palabras_por_vocabulario
+# from bson import ObjectId
+from fastapi.middleware.cors import CORSMiddleware
+
+# from dotenv import load_dotenv
+
+
+# load_dotenv()
+
+
+app = FastAPI()
+
+# app.include_router(palabras.router)
+# app.include_router(usuarios.router)
+
+
+origins = [
+    "http://localhost:8080",
+    # "https://vocabularios.netlify.app"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+async def api_portfolio():
+    return {"API PORTFOLIO": "This a portfolio API. Web portfolio is a collection of works or projects that have been completed in the field of web design or web development, including information about the web designer or developer and their experience, as well as examples of projects they have worked on, services provided, skills and contact information"}
+
+
+
+
+# uvicorn main:app --reload
